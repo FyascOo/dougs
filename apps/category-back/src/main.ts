f734da0230be8e87c2f9623eacd886a46a040ef9
Fyasco <alan.choufa@gmail.com>
2024-05-31 22:52:11 +0200

    chore: add node backend project
    nx add @nx/node
    nx g @nx/node:application category-back

---
+++ apps/category-back/src/main.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { categories } from './categories';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const app = express();
+
+app.get('/all-categories', (req, res) => {
+  res.json(categories);
+});
+
+app.get('/visible-categories', (req, res) => {
+  const fakeVisibleCategories = categories
+    .filter((category) => category.id % 3 === 0)
+    .map(({ id }) => ({ id }));
+
+  res.json(fakeVisibleCategories);
+});
+
+app.get('*', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
